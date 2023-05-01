ca30f2ba5712a1be4ffadddddd81e69fedd0280e
Anthony <aquinoanthony851@gmail.com>
2023-04-30 18:32:38 -0600

    Events Acting On Events

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
-  }
-}
+  };
+};
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10 );
+  
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  };
+};
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
-  }
-});
-
-function moveDodgerRight() {
-  const rightNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(rightNumbers, 10);
-
-  if (left < 360) {
-    dodger.style.left = `${left + 1}px`;
-  }
-}
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowRight") {
+  } else if (e.key === "ArrowRight") {
     moveDodgerRight();
   }
 });
+
